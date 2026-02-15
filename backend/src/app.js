0ad442d10a6ae7482dd7ec4c71629e548eba18ca
Ravinder Tiwari <ravindertiwari373@gmail.com>
2026-02-15 20:42:51 +0530

    commit2

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,11 +1,17 @@
 const express = require('express')
 const aiRoutes = require("./routes/ai.routes")
 const app = express()
+const path = require("path")
 const cors = require("cors")
 app.use(cors())
 app.use(express.json())
+app.use(express.static("./public"))
 app.get("/",(req,res)=>{
     res.send("hello world")
 })
 app.use("/ai",aiRoutes)
+
+app.use('*name',(req,res)=>{
+    res.sendFile(path.join(__dirname,"..","/public/index.html"))
+})
 module.exports = app;
